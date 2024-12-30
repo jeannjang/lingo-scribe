886b1f4058f7aef7ecc7aaa85736b99302f1626f
Jeahoon Jung <jhoon5245@gmail.com>
2024-12-30 15:48:18 +1300

    #15 Injected pageScript to execute in the page's context
    Added a pageScript to access `window.JSON.parse` for parsing subtitle metadata and `window.netflix` for interacting with the Netflix video object for video control. The `contentScript` injects this `pageScript` into the DOM, enabling it to run within the page's context.

---
+++ src/pageScript/pageScript.ts
@@ -0,0 +1,4 @@
+// This code is injected into the page by contentScript.ts,
+// and it is executed in the page's context, unlike contentScript.ts and serviceWorker.ts.
+
+console.log('Hello from pageScript.ts!');
