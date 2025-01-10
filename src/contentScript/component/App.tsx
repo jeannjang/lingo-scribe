ba9d7e8c4ac135d89ff86585456f5e1f47f7c381
Jeahoon Jung <jhoon5245@gmail.com>
2025-01-10 20:57:59 +1300

    #29 Updated contentScript to load on netflix.com and show react app only when window is on a video player page

--- src/contentScript/component/App.tsx
+++ src/contentScript/component/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserPreferences } from './store/userSlice';
 import { RootState, StoreDispatch } from './store/store';
 import InitialLanguageSelectModal from '@/src/contentScript/component/components/InitialLanguageSelectModal';
+import { determineIfWindowOnPlayerPage } from '@/src/pageScript/spyOnPageUrl';
+import { setIsWindowOnPlayerPage } from '@/src/contentScript/component/store/appSlice';
 
 const App = () => {
     const dispatch = useDispatch<StoreDispatch>();
     const userState = useSelector((state: RootState) => state.user);
+    const isWindowOnPlayerPage = useSelector(
+        (state: RootState) => state.app.isWindowOnPlayerPage
+    );
+
+    // When App is loaded firs time, we should check if the window is on videoPlayer page or not.
+    useEffect(() => {
+        const currentUrl = window.location.href;
+        dispatch(
+            setIsWindowOnPlayerPage({
+                isWindowOnPlayerPage: determineIfWindowOnPlayerPage(currentUrl),
+            })
+        );
+    }, []);
+
+    if (!isWindowOnPlayerPage) {
+        return null;
+    }
 
     if (userState.status === 'idle') {
         dispatch(fetchUserPreferences());
