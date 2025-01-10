7a4d929f8135ba2da823fefdb7e39e6a72ce167d
Jeahoon Jung <jhoon5245@gmail.com>
2025-01-10 20:51:15 +1300

    #29 Added appSlice for indicating application status

--- src/contentScript/component/store/store.ts
+++ src/contentScript/component/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './appSlice';
 import subtitleReducer from './subtitleSlice';
 import userReducer from './userSlice';
 
 export const store = configureStore({
     reducer: {
+        app: appReducer,
         subtitle: subtitleReducer,
         user: userReducer,
     },
