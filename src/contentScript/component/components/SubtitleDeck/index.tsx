c512a1057947ea023d468c57d456fdc3fd29794a
Jean <jeanjyjh@gmail.com>
2025-02-09 13:46:47 +1300

    #48 Add dictation and dual subtitle mode switching functionality
    - Created 'DeckMode' type ('dictation' | 'dualSub') to manage different subtitle display modes
    - Create DeckModeSwitch.tsx component with mode toggling using shadcn/ui
    - Modify SubtitleBar.tsx to handle different visibility states based on mode

--- src/contentScript/component/components/SubtitleDeck/index.tsx
+++ src/contentScript/component/components/SubtitleDeck/index.tsx
@@ -3,12 +3,14 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@/src/contentScript/component/store/store';
 import SubtitleBar from '@/src/contentScript/component/components/SubtitleDeck/SubtitleBar';
 import DictationTextInput from '@/src/contentScript/component/components/SubtitleDeck/DictationTextInput';
-
+import DeckModeSwitch from '@/src/contentScript/component/components/SubtitleDeck/DeckModeSwitch';
+export type DeckMode = 'dictation' | 'dualSub';
 const SubtitleDeck = () => {
     const subtitles = useSelector(
         (state: RootState) => state.subtitle.subtitles
     );
     const [isUserAnswerChecking, setIsUserAnswerChecking] = useState(false);
+    const [mode, setMode] = useState<DeckMode>('dictation');
 
     console.log('All subtitles:', subtitles);
 
@@ -16,20 +18,33 @@ const SubtitleDeck = () => {
         <>
             <div
                 className={
-                    'absolute bottom-44 w-full flex flex-col items-center justify-center space-y-2 pointer-events-auto'
+                    'absolute bottom-44 w-full flex flex-col items-center justify-center space-y-1.5 pointer-events-auto'
                 }
             >
-                {subtitles.study ? (
+                {subtitles.study && (
                     <SubtitleBar
                         subtitle={subtitles.study}
                         isUserAnswerChecking={isUserAnswerChecking}
+                        mode={mode}
                     />
-                ) : null}
-                {/*<SubtitleBar subtitle={subtitles.guide}/>*/}
-                <DictationTextInput
-                    subtitle={subtitles.study}
-                    setIsUserAnswerChecking={setIsUserAnswerChecking}
-                />
+                )}
+                {subtitles.guide && (
+                    <SubtitleBar
+                        subtitle={subtitles.guide}
+                        isUserAnswerChecking={isUserAnswerChecking}
+                        mode={mode}
+                    />
+                )}
+
+                {mode === 'dictation' && (
+                    //Control video playback based on the study subtitle timestamps.
+                    <DictationTextInput
+                        subtitle={subtitles.study}
+                        setIsUserAnswerChecking={setIsUserAnswerChecking}
+                    />
+                )}
+
+                <DeckModeSwitch mode={mode} setModeChange={setMode} />
             </div>
         </>
     );
