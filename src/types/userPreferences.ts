496f0737d67b995dedcdab8ca1677e01096b82df
Jean <jeanjyjh@gmail.com>
2025-02-24 11:26:00 +1300

    #50 Add types for user preferences
    - Add types for user preferences, which will be used in communication between popup, contentScript and serviceWorker.
    - Extract DeckMode type for better reusability.
    - Plus, a partial of shadcn feature is updated.

--- src/types/userPreferences.ts
+++ src/types/userPreferences.ts
@@ -1,10 +1,14 @@
 interface UserPreferencesV1 {
     version: 1;
     isAppEnabled: boolean;
-    // The BCP 47 language tag of the language the user is studying.
+    // The BCP 47 language tag of the language the user is studying. : use in comboBox
     studyLanguage: string | undefined;
-    // The BCP 47 language tag of the language the user wants to see the guide in.
+    // The BCP 47 language tag of the language the user wants to see the guide in : use in comboBox
     guideLanguage: string | undefined;
+    deckMode: DeckMode;
+    isAutoPause: boolean;
 }
+
+export type DeckMode = 'dictation' | 'dualSub';
 
 export type UserPreferences = UserPreferencesV1;
