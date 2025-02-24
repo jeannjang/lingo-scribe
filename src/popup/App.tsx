6f7877cdacf2d9edeac03c41ddb676ab85391767
Jean <jeanjyjh@gmail.com>
2025-02-24 13:32:25 +1300

    #50 Integrate PopupCard.tsx with serviceWorker.ts
     - Implement message passing between popup and service worker:
       - Popup state changes trigger messages to service worker
     - Configure initial state handling in App.tsx
     - Handle state updates in popup component

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PopupCard from '@/src/popup/PopupCard';
+import { UserPreferences } from '@/src/types/userPreferences';
+import {
+    GetUserPreferencesRequest,
+    GetUserPreferencesResponse,
+    messageType,
+    UserPreferencesUpdated,
+} from '@/src/types/messages';
 
+// Manage user preference state and state updates
+// Inside useEffect, send a message to the service worker and update the state with the response
+// Pass the state (value) as a prop to PopupCard
 const App = () => {
-    return <PopupCard />;
+    const [userPreferences, setUserPreferences] = useState<
+        UserPreferences | undefined
+    >(undefined);
+
+    // On initial render, send a message to retrieve stored preferences
+    useEffect(() => {
+        chrome.runtime
+            .sendMessage<GetUserPreferencesRequest, GetUserPreferencesResponse>(
+                {
+                    type: messageType.getUserPreferences,
+                }
+            )
+            .then((userPreferencesResponse) => {
+                setUserPreferences(userPreferencesResponse);
+            });
+    }, []);
+
+    // The service worker triggers the setUserPreferences() function in IndexedDB
+    // and stores the preferences, and sends back a response
+    // App.tsx update PopupCard with the received message
+    useEffect(() => {
+        chrome.runtime.onMessage.addListener((message) => {
+            if (message.type === messageType.userPreferencesUpdated) {
+                setUserPreferences(
+                    (message as UserPreferencesUpdated).payload.userPreferences
+                );
+            }
+        });
+    }, []);
+
+    return <PopupCard userPreferences={userPreferences} />;
 };
 
 export default App;
--- src/popup/PopupCard.tsx
+++ src/popup/PopupCard.tsx
@@ -1,28 +1,103 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Switch } from '@/components/ui/switch';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ComboBox from '@/src/contentScript/component/components/InitialLanguageSelectModal/ComboBox';
+import { DeckMode, UserPreferences } from '@/src/types/userPreferences';
+import { messageType } from '@/src/types/messages';
 
-const PopupCard = () => {
-    const [studyLanguage, setStudyLanguage] = useState<string | undefined>(
-        undefined
-    );
-    const [guideLanguage, setGuideLanguage] = useState<string | undefined>(
-        undefined
-    );
+interface IProps {
+    userPreferences: UserPreferences | undefined;
+}
+
+const PopupCard: React.FC<IProps> = ({ userPreferences }) => {
+    if (!userPreferences) {
+        return null;
+    }
+
+    const handleAppEnabledChange = (isEnabled: boolean) => {
+        const newUserPreferences = {
+            ...userPreferences,
+            isAppEnabled: isEnabled,
+        };
+
+        chrome.runtime.sendMessage({
+            type: messageType.setUserPreferences,
+            payload: { userPreferences: newUserPreferences },
+        });
+    };
+
+    const handleDeckModeChange = (newDeckModeValue: DeckMode) => {
+        const newUserPreferences = {
+            ...userPreferences,
+            deckMode: newDeckModeValue,
+            isAutoPause:
+                newDeckModeValue === 'dictation'
+                    ? true
+                    : userPreferences.isAutoPause,
+        };
+        chrome.runtime.sendMessage({
+            type: messageType.setUserPreferences,
+            payload: { userPreferences: newUserPreferences },
+        });
+    };
+
+    const handleStudyLanguageChange = (language: string) => {
+        const newUserPreferences = {
+            ...userPreferences,
+            studyLanguage: language,
+        };
+
+        chrome.runtime.sendMessage({
+            type: messageType.setUserPreferences,
+            payload: { userPreferences: newUserPreferences },
+        });
+    };
+
+    const handleGuideLanguageChange = (language: string) => {
+        const newUserPreferences = {
+            ...userPreferences,
+            guideLanguage: language,
+        };
+
+        chrome.runtime.sendMessage({
+            type: messageType.setUserPreferences,
+            payload: { userPreferences: newUserPreferences },
+        });
+    };
+
+    const handleIsAutoPauseChange = (isAutoPaused: boolean) => {
+        const newUserPreferences = {
+            ...userPreferences,
+            isAutoPause: isAutoPaused,
+        };
+
+        chrome.runtime.sendMessage({
+            type: messageType.setUserPreferences,
+            payload: { userPreferences: newUserPreferences },
+        });
+    };
 
     return (
         <div className={'w-[330px] flex flex-col p-5'}>
             {/* A: Title and App Switch*/}
             <div className={'flex items-center justify-between mb-10'}>
                 <h1 className={'text-xl font-semibold'}>LINGO SCRIBE</h1>
-                <Switch id={'app-switch'} />
+                <Switch
+                    id={'app-switch'}
+                    checked={userPreferences.isAppEnabled}
+                    onCheckedChange={handleAppEnabledChange}
+                />
             </div>
 
             {/* B: Set Mode Tabs*/}
             <div>
                 <h2 className={'text-lg font-semibold mb-2'}>Set Mode</h2>
-                <Tabs defaultValue={'dictation'}>
+                <Tabs
+                    value={userPreferences.deckMode}
+                    onValueChange={(newDeckModeValue) =>
+                        handleDeckModeChange(newDeckModeValue as DeckMode)
+                    }
+                >
                     <TabsList className={'w-full grid grid-cols-2 mb-10'}>
                         <TabsTrigger value={'dictation'}>Dictation</TabsTrigger>
                         <TabsTrigger value={'dualSub'}>Dual Sub</TabsTrigger>
@@ -34,8 +109,8 @@ const PopupCard = () => {
                                 Study Language
                             </h2>
                             <ComboBox
-                                value={studyLanguage}
-                                onSelect={setStudyLanguage}
+                                value={userPreferences?.studyLanguage}
+                                onSelect={handleStudyLanguageChange}
                             />
                             <p>This will be your target language.</p>
                         </div>
@@ -46,8 +121,8 @@ const PopupCard = () => {
                                 Guide Language
                             </h2>
                             <ComboBox
-                                value={guideLanguage}
-                                onSelect={setGuideLanguage}
+                                value={userPreferences?.guideLanguage}
+                                onSelect={handleGuideLanguageChange}
                             />
                             <p>This will be your native language.</p>
                         </div>
@@ -60,21 +135,28 @@ const PopupCard = () => {
                                 <h2 className={'text-lg font-semibold'}>
                                     Auto Pause
                                 </h2>
-                                <Switch disabled aria-readonly />
+                                <Switch
+                                    disabled
+                                    checked={userPreferences.isAutoPause}
+                                    aria-readonly
+                                />
                             </div>
                             <p>AP is always on in dictation mode</p>
                         </div>
                     </TabsContent>
 
-                    <TabsContent value={'dualSub'} className={'space-y-10'}>
+                    <TabsContent
+                        value={'dualSub' as DeckMode}
+                        className={'space-y-10'}
+                    >
                         {/* C: Select Study Language*/}
                         <div className={'space-y-2'}>
                             <h2 className={'text-lg font-semibold'}>
                                 Study Language
                             </h2>
                             <ComboBox
-                                value={studyLanguage}
-                                onSelect={setStudyLanguage}
+                                value={userPreferences?.studyLanguage}
+                                onSelect={handleStudyLanguageChange}
                             />
                             <p>This will be your target language.</p>
                         </div>
@@ -85,8 +167,8 @@ const PopupCard = () => {
                                 Guide Language
                             </h2>
                             <ComboBox
-                                value={guideLanguage}
-                                onSelect={setGuideLanguage}
+                                value={userPreferences?.guideLanguage}
+                                onSelect={handleGuideLanguageChange}
                             />
                             <p>This will be your native language.</p>
                         </div>
@@ -100,7 +182,11 @@ const PopupCard = () => {
                                     Auto Pause
                                 </h2>
 
-                                <Switch id={'ap-switch'} />
+                                <Switch
+                                    id={'ap-switch'}
+                                    checked={userPreferences.isAutoPause}
+                                    onCheckedChange={handleIsAutoPauseChange}
+                                />
                             </div>
                         </div>
                     </TabsContent>
